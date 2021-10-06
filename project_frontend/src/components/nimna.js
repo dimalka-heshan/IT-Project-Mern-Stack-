7fa3eaec8aa107ca8a42c95390f0f08d7ac2abd4
Nimna Thiranjaya <nimnathiranjaya523@gmail.com>
2021-10-06 18:56:36 +0530

    test1

---
+++ project_frontend/src/components/nimna.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class nimna extends Component {
+    render() {
+        return (
+            <div>
+                <> test</>
+            </div>
+        )
+    }
+}
